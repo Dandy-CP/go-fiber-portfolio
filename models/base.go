9fcdd2089738c1613f1e83c405fbeeccc6e23c34
Dandy Candra <dandycandrapratama@gmail.com>
2023-10-28 14:09:54 +0700

    add base model

---
+++ models/base.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type (
+	Base struct {
+		ID        uuid.UUID  `gorm:"type:uuid;primary_key;"`
+		CreatedAt time.Time  `json:"created_at"`
+		UpdatedAt time.Time  `json:"update_at"`
+		DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+	}
+
+	ErrorResponse struct {
+		Error       bool
+		FailedField string
+		Tag         string
+		Value       interface{}
+	}
+)
