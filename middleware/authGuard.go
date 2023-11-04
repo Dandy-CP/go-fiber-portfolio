e207f8d53fc9a8d9aecf854885bdb03b37f58f7a
Dandy Candra <dandycandrapratama@gmail.com>
2023-11-04 12:14:06 +0700

    add auth guard function middleware

---
+++ middleware/authGuard.go
@@ -0,0 +1,60 @@
+package middleware
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Dandy-CP/go-fiber-portfolio/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthGuard(c *fiber.Ctx) error {
+	var tokenString string
+	authorization := c.Get("Authorization")
+
+	if strings.HasPrefix(authorization, "Bearer ") {
+		tokenString = strings.TrimPrefix(authorization, "Bearer ")
+	}
+
+	if tokenString == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"Status":  fiber.StatusUnauthorized,
+			"message": "Unauthorized",
+		})
+	}
+
+	tokenByte, err := jwt.Parse(tokenString, func(jwtToken *jwt.Token) (interface{}, error) {
+		if _, ok := jwtToken.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", jwtToken.Header["alg"])
+		}
+		return []byte("nffhdufnxh44ffefs"), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": fiber.StatusUnauthorized, 
+			"message": fmt.Sprintf("invalidate token: %v", err),
+		})
+	}
+
+	claims, ok := tokenByte.Claims.(jwt.MapClaims)
+	if !ok || !tokenByte.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": fiber.StatusUnauthorized, 
+			"message": "invalid token claim",
+		})
+	}
+
+	var user models.User
+	models.DB.First(&user, "id = ?", fmt.Sprint(claims["sub"]))
+
+	if user.ID.String() != claims["sub"] {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"status": fiber.StatusForbidden,
+			"message": "the user belonging to this token no logger exists",
+		})
+	}
+
+	return c.Next()
+}
