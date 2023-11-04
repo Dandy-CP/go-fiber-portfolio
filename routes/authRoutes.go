9b1b8fcc6ad4ef89b57ff89a62fe8da1218ecb80
Dandy Candra <dandycandrapratama@gmail.com>
2023-11-04 12:16:07 +0700

    add auth routes

---
+++ routes/authRoutes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	authusercontroller "github.com/Dandy-CP/go-fiber-portfolio/controllers/AuthUserController"
+	"github.com/Dandy-CP/go-fiber-portfolio/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthRoutes(auth fiber.Router) {
+	auth.Post("/login",
+		middleware.ValidateAuth,
+			authusercontroller.AuthLogin)
+
+	auth.Post("/register",
+		middleware.ValidateAuth,
+			authusercontroller.AuthSignUp)
+}
