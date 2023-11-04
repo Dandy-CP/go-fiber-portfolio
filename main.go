6b3afab9c44def114649d600b47029abfc685132
Dandy Candra <dandycandrapratama@gmail.com>
2023-11-04 16:23:34 +0700

    implement env init, add fiber logger, fiber cors and wild card route to 404

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"github.com/Dandy-CP/go-fiber-portfolio/models"
+	"fmt"
+	"log"
+
+	"github.com/Dandy-CP/go-fiber-portfolio/config"
 	"github.com/Dandy-CP/go-fiber-portfolio/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+func init() {
+	setup, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatalln("Failed to load environment variables! \n", err.Error())
+	}
+
+	config.ConnectDB(&setup)
+}
+
 func main() {
-	models.ConnectDB()
 	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, PUT, DELETE",
+	}))
 
 	api := app.Group("/api")
 	myProjects := api.Group("/my-projects")
@@ -28,6 +48,23 @@ func main() {
 	routes.BlogRoutes(blog)
 	routes.CertificateRoutes(certificate)
 	routes.AuthRoutes(auth)
+
+	app.Get("*", func(c *fiber.Ctx) error {
+		path := c.Path()
+
+		if path == "/api" {
+			return c.JSON(fiber.Map{
+				"statusCode": 200,
+				"message": "Welcome To My Portfolio Rest API",
+				"version": "0.0.1",
+			})
+		}
+
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"status":  "fail",
+			"message": fmt.Sprintf("Path: %v does not exists on this server", path),
+		})
+	})
 
 	if err := app.Listen(":8000"); err != nil {
 		panic(err)
